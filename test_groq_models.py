e59a7cccd2c2f457abc609c994eb1a5aeb0c6f2d
Byom deep <byomdeepbisaria@gmail.com>
2026-09-22 01:26:52 +0530

    Remove temporary scratch scripts

--- test_groq_models.py
+++
@@ -1,8 +0,0 @@
-﻿import os
-import sys
-from groq import Groq
-from dotenv import load_dotenv
-
-load_dotenv('apps/api/.env')
-client = Groq(api_key=os.environ["GROQ_API_KEY"])
-print([m.id for m in client.models.list().data])
